40a2342464f98e61e2f3d35939324250dd2f8315
mgarcia <miguelglafuente@gmail.com>
2016-01-12 12:25:21 +0100

    Send data selected via wiring (new output endpoint). resolve #1

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,35 @@ window.onload = function () {
 
     $("#container").highcharts({});
 
+    var sendData = function sendData(data) {
+        var toSend = {};
+        toSend.category = data.category || "";
+        toSend.percentage = data.percentage || 0.0;
+        toSend.x = data.x;
+        toSend.y = data.y;
+        MashupPlatform.wiring.pushEvent("selected", JSON.stringify(toSend));
+    };
+
+    var hijackEvents = function hijackEvents(data) {
+        data.plotOptions = data.plotOptions || {};
+        data.plotOptions.series = data.plotOptions.series || {};
+        data.plotOptions.series.point = data.plotOptions.series.point || {};
+        data.plotOptions.series.point.events = {}; // Let's clean all the events because can't be sended via wiring
+        data.plotOptions.series.point.events.click = function (e) {
+            sendData(this); // this :: Point
+        };
+        return data;
+    };
+
     MashupPlatform.wiring.registerCallback("highcharts", function (data) {
         var jdata = JSON.parse(data);
+        jdata = hijackEvents(jdata);
         $("#container").highcharts(jdata);
     });
 
     MashupPlatform.wiring.registerCallback("highstock", function (data) {
         var jdata = JSON.parse(data);
+        jdata = hijackEvents(jdata);
         $("#container").highcharts("StockChart", jdata);
     });
 };
